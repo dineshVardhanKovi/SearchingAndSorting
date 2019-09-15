81a2ce16aad5b1e458c272f7f71e663955f967e7
dineshVardhanKovi <dineshVardhanKovi@users.noreply.github.com>
2019-09-15 17:52:58 +0530

    searching

---
+++ searchingAndSorting/src/dinesh/searchingAndSorting/arrays/Searching.java
@@ -0,0 +1,82 @@
+package dinesh.searchingAndSorting.arrays;
+
+public class Searching {
+	public static void main(String[] args) {
+		int[] array = {1,2,3,4,5,6,7,8,9};
+		//System.out.println(linearSearch(array, 6));
+		//System.out.println(binarySearchIterative(array, 31));
+		//System.out.println(binarySearchRecursive(array, 31,0,array.length-1));
+		System.out.println(jumpSearch(array, 9));
+
+	}
+
+	//linear search
+	public static boolean linearSearch(int[] array,int x) {
+		for (int i = 0; i < array.length; i++) {
+			if(array[i] == x) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//iterative binary search
+	public static boolean binarySearchIterative(int[] array,int x) {
+		int low = 0;
+		int high = array.length-1;
+		while(low <= high) {
+			int mid = low+(high-1)/2;
+			if(array[mid] == x) {
+				return true;
+			}else if (array[mid] < x) {
+				low = mid +1;
+			}else {
+				high = mid -1;
+			}
+		}		
+		return false;
+	}
+	
+	//recursive binary search
+	public static boolean binarySearchRecursive(int[] array,int x,int low,int high) {
+		while(low <= high) {
+			int mid = low+(high-1)/2;
+			if(array[mid] == x) {
+				return true;
+			}else if (array[mid] < x) {
+				return binarySearchRecursive(array, x, mid + 1,high);
+			}else {
+				return binarySearchRecursive(array, x, low,mid -1 );
+			}
+		}		
+		return false;
+	}
+	
+	//jump search
+	public static boolean jumpSearch(int[] array,int x) {
+		int prev = 0;
+		int n = array.length;
+		int step = (int) Math.sqrt(n);
+		
+		//finding out the interval
+		while(array[Math.min(step,n)-1] < x) {
+			prev = step;
+			step += step;
+			if(prev >= n) {
+				return false;
+			}
+		}
+		
+		//doing a linear search
+		while(array[prev] <x) {
+			prev++;
+			if(prev == Math.min(step, n)) {
+				return false;
+			}
+		}
+		if(array[prev] == x) {
+			return true;
+		}
+		return false;
+	}
+}
